dad6435e1eafc7f1481fa3b5eb70d39fd56f9b11
Gokul <110253999+Gokulv96@users.noreply.github.com>
2023-08-01 20:28:13 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,17 @@
+1]
+class Movies{
+     constructor(movie,studio,rating="PG")
+     {
+    this.movie=movie;
+    this.studio=studio;
+    this.rating=rating;
+    }
+}
+a)var obj=new Movies("Titanic","Dolby"," PG­13");
+console.log(obj);
+
+b) var obj=new Movies("Titanic","Dolby");
+  console.log(obj);
+
+d)var obj=new Movies("Casino Royale",“Eon Productions”,“PG­13”);
+  console.log(obj);
