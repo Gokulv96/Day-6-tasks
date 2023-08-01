251f98ee2430c74d8d844c4fe615cc5f20ee588f
Gokul <110253999+Gokulv96@users.noreply.github.com>
2023-08-01 20:59:29 +0530

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,11 @@
+class Details{
+  constructor(name,age,mobileno,city)
+  {
+    this.name=name;
+    this.age=age;
+    this.mobileno=mobileno;
+    this.city=city;
+  }
+}
+var obj=new Details("Gokul","22","123456788","salem");
+console.log(obj);
